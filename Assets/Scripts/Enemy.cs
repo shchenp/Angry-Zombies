4f5951ce86331f97f79d7042086844d1965ab3bf
Nikita <np.shche@gmail.com>
2025-02-26 21:45:49 +0300

    initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    [SerializeField] 
+    private AudioClip _deathSound;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // TODO: Напишите логику уничтожения зомби тут
+    }
+
+    private void Die()
+    {
+        // Создаем эффект "взрыв" на месте убитого зомби.
+        CreateExplosion();
+        // ПРоигрываем звук смерти зомби.
+        PlayDeathSound();
+        // Разрушаем объект зомби.
+        Destroy(gameObject);
+    }
+
+    private void PlayDeathSound()
+    {
+        AudioSource.PlayClipAtPoint(_deathSound, transform.position);
+    }
+    
+    private void CreateExplosion()
+    {
+        Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+    }
+}
